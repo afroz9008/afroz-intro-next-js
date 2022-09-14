f971985222143ee890a13a57b700faf9fd1043b9
afroz9008 <54906553+afroz9008@users.noreply.github.com>
2022-09-14 13:48:29 +0530

    spell correct

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="/static/favicon.ico" />
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2220577119913325" crossorigin="anonymous"></script>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2220577119913325" crossOrigin="anonymous"></script>
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {this.props.emotionStyleTags}
         </Head>
