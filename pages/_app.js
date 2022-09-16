295b07983d8654b34c7b1a89d19a68c026a14303
afroz9008 <54906553+afroz9008@users.noreply.github.com>
2022-09-16 19:21:16 +0530

    add setting button

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ import "slick-carousel/slick/slick-theme.css";
 import '../styles/app.scss'
 import '../styles/simple-line-icons.css';
 import Layout from '../Components/Layout';
+import { IconButton } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -18,6 +21,13 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </>
     </Layout>
+    <IconButton style={{ position: "fixed", right: 0, bottom: 0, outline: 'none' }}>
+      <Link href="https://afroz-sorathiya.sanity.studio" passHref>
+        <a target="_blank" rel="noopener noreferrer" style={{ height: 50, width: 50 }}>
+          <Image src={"/settings.png"} height={50} width={50} alt='Admin Setting' />
+        </a>
+      </Link>
+    </IconButton>
   </CacheProvider>
 }
 
