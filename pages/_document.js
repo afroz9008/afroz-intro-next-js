acaac8559d12bf78694f4c30776610a24393004b
afroz9008 <54906553+afroz9008@users.noreply.github.com>
2023-05-27 17:04:58 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="/static/favicon.ico" />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4765929126302215" crossorigin="anonymous"></script>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4765929126302215" crossOrigin="anonymous"></script>
           {this.props.emotionStyleTags}
         </Head>
         <body>
