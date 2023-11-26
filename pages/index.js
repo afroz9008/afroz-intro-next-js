faa5bc4e9695cd7354564858d50ef115a6f46202
afroz9008 <54906553+afroz9008@users.noreply.github.com>
2023-11-26 17:23:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,47 +9,62 @@ import Experiences from '../Components/Experiences';
 import Clients from '../Components/Clients';
 import Contact from '../Components/Contact';
 import { getAboutSectionData, getExperinece, getHeader, getHeroSectionData, getServices, getSkills } from '../libs/queries';
+import Layout from '../Components/Layout';
+import { IconButton } from '@mui/material';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home(props) {
 
   return (
     <>
-      <Head>
-        <title>{props.header.title}</title>
-        <meta name="description" content={`${props.header.title} - ${props?.header?.designation}`} />
-        <link rel="icon" href={props.header.image} />
-      </Head>
-      <Element name="section-home">
-        <Hero data={props.hero} />
-      </Element>
+      <Layout header={props.header}>
+        <>
+          <Head>
+            <title>{props.header.title}</title>
+            <meta name="description" content={`${props.header.title} - ${props?.header?.designation}`} />
+            <link rel="icon" href={props.header.image} />
+          </Head>
+          <Element name="section-home">
+            <Hero data={props.hero} />
+          </Element>
 
-      <Element name="section-about">
-        <About data={props.about} />
-      </Element>
+          <Element name="section-about">
+            <About data={props.about} />
+          </Element>
 
-      <Element name="section-skills">
-        <Skills data={props.skills} about={props.about} />
-      </Element>
+          <Element name="section-skills">
+            <Skills data={props.skills} about={props.about} />
+          </Element>
 
-      <Element name="section-funfacts">
-        <Funfacts />
-      </Element>
+          <Element name="section-funfacts">
+            <Funfacts />
+          </Element>
 
-      <Element name="section-services">
-        <Services data={props.services} />
-      </Element>
+          <Element name="section-services">
+            <Services data={props.services} />
+          </Element>
 
-      <Element name="section-experiences">
-        <Experiences data={props.experiences} />
-      </Element>
+          <Element name="section-experiences">
+            <Experiences data={props.experiences} />
+          </Element>
 
-      <Element name="section-clients">
-        <Clients />
-      </Element>
+          <Element name="section-clients">
+            <Clients />
+          </Element>
 
-      <Element name="section-contact">
-        <Contact data={props.about} />
-      </Element>
+          <Element name="section-contact">
+            <Contact data={props.about} />
+          </Element>
+        </>
+      </Layout>
+      <IconButton style={{ position: "fixed", right: 0, bottom: 0, outline: 'none' }}>
+        <Link href="https://afroz-sorathiya.sanity.studio" passHref>
+          <a target="_blank" rel="noopener noreferrer" style={{ height: 50, width: 50 }}>
+            <Image src={"/settings.png"} height={50} width={50} alt='Admin Setting' />
+          </a>
+        </Link>
+      </IconButton>
     </>
   )
 }
