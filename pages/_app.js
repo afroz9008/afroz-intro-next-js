c02d8b803c197be9f7f40e0f150490d703558974
afroz9008 <54906553+afroz9008@users.noreply.github.com>
2023-11-26 17:23:00 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,28 +6,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../styles/app.scss'
 import '../styles/simple-line-icons.css';
-import Layout from '../Components/Layout';
-import { IconButton } from '@mui/material';
-import Image from 'next/image';
-import Link from 'next/link';
 
 const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps }) {
 
   return <CacheProvider value={clientSideEmotionCache}>
-    <Layout header={pageProps.header}>
-      <>
-        <Component {...pageProps} />
-      </>
-    </Layout>
-    <IconButton style={{ position: "fixed", right: 0, bottom: 0, outline: 'none' }}>
-      <Link href="https://afroz-sorathiya.sanity.studio" passHref>
-        <a target="_blank" rel="noopener noreferrer" style={{ height: 50, width: 50 }}>
-          <Image src={"/settings.png"} height={50} width={50} alt='Admin Setting' />
-        </a>
-      </Link>
-    </IconButton>
+    <Component {...pageProps} />
   </CacheProvider>
 }
 
